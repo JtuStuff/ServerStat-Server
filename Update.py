7bab131d173fc784d5c883e60bcd53d96c79b1d6
FlyingThaCat <95353217+FlyingThaCat@users.noreply.github.com>
2022-03-27 19:46:26 +0700

    Update Update.py
    Fix bug hopefully

--- Update.py
+++ Update.py
@@ -1,28 +1,23 @@
 import requests as rq
 from ping3 import ping
 
-ServerUrl = ""
+#https://github.com/JtuStuff/ServerStat-Client
+ServerUrl = "YOUR URL TO UPDATE"
 
 #Set Header
 headers = {'User-Agent': 'ServerStatClient/1.0'}
 #Get request to get external ip
 pi = rq.get("http://ipv4.icanhazip.com").content.decode('utf8').rstrip("\n")
 #Grouping the return 
-def myping(host):
-    resp = ping(host)
-
-    if resp == False:
-        return False
-    else:
-        return True
+pong = ping(pi)
+if pong != None:
+    data = {'Status':'Online'}
+else:
+    data = {'Status':'Offline'}
 
 #Print Note
 print("\nPlz this is may not accurate or can make many problem")
 #Building post data
-if myping(pi) == False :
-    data = {'Status':'Offline'}
-else :
-    data = {'Status':'Online'}
 
 #Post it
 session = rq.Session()
